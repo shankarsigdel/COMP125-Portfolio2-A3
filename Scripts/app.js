7f5f3fc92766c488f8bb720fb159a857561b249b
LAPTOP-NKGJ4DT9\Admin <sss.sgd26@gmail.com>
2020-08-07 17:20:22 -0400

    changes

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,78 +1,74 @@
-// IIFE -Immediately Ivoked Function Expression
-/*    
-----JavaScript --------
+/*
 Shankar Sigdel
-Assignment 3 Extended mini Portofolio
-student ID: 301110925
-Client Side Web
-Professor: Tom Tsiliopoulos
+Assignment3
+COMP125 M2020
 */
-
 "use strict";
 
 displayOutput();
 function displayOutput()
 {
     console.log('%cCOMP125-Assignment3', "color:ivory; background-color:purple ;font-size: 24px;");   
-    let title = document.title;
-    title = title.toLowerCase();
-    console.log(`The title of the page is ${title}`);
+   let title = document.title;
+   title = title.toLowerCase();
+   console.log(`The title of the page is ${title}`);
 }
- //This is for nav..header and footer
-    function setNavData()
-{
-     if (document.title == "home.html" || 
-     document.title == "contact.html" || 
-     document.title == "project.html") 
-    {
-    loadHeader();
-    loadFooter();      
-    }                        
-}  
-    let xhrContent;
 
-    //Reads the content from JSON 
-    function readParagraphs() 
+ //This is for nav..header and footer
+function setNavData()
+{
+ 
+if (document.title == "home.html" || document.title == "contact.html" || document.title == "project.html") 
+    {
+        loadHeader();
+        loadFooter();      
+    }                        
+} 
+     //Header Part
+    loadHeader();
+    function loadHeader()
+    {
+        console.info("Header is loading...");
+        // step 1 - Creates the XHR object
+        let XHR = new XMLHttpRequest();
+                    
+        // step 2 - Configures the message
+        XHR.open("GET", "./Views/partials/header.html");
+                    
+        // step 3 - Executes the request
+        XHR.send();
+                    
+        XHR.addEventListener("readystatechange", function()
+        {
+            if((XHR.readyState === 4) && (XHR.status === 200))
+            {
+                let header = document.getElementsByTagName("header")[0];
+                    
+                 let headerData = XHR.responseText;
+                    
+                 header.innerHTML = headerData;
+            }
+        });   
+    } 
+
+let xhrContent;
+//Reads the content from JSON 
+function readParagraphs() 
 {
     if ((xhrContent.readyState === 4) && (xhrContent.status === 200)) 
       {
-    let MyContent = JSON.parse(xhrContent.responseText);
-    let paragraphs = MyContent.paragraphs;
-    paragraphs.forEach(function (paragraph) 
-        {
-        let paragraphElements = document.getElementById(paragraph.id);
+          let MyContent = JSON.parse(xhrContent.responseText);
+          let paragraphs = MyContent.paragraphs;
+          paragraphs.forEach(function (paragraph) 
+            {
+                let paragraphElements = document.getElementById(paragraph.id);
                 
-        if(paragraphElements) 
-        {
-             paragraphElements.innerHTML = paragraph.content;
-        }               
-        }, this);
-    }
-}
-    loadHeader();
-    function loadHeader()
-{
-  console.info("Header is loading...");
-  // step 1 - Creates the XHR object
-  let XHR = new XMLHttpRequest();
-
-  // step 2 - Configures the message
-  XHR.open("GET", "./Views/partials/header.html");
-
-  // step 3 - Executes the request
-  XHR.send();
-
-  XHR.addEventListener("readystatechange", function()
-  {
-      if((XHR.readyState === 4) && (XHR.status === 200))
-      {
-          let header = document.getElementsByTagName("header")[0];
-
-          let headerData = XHR.responseText;
-
-          header.innerHTML = headerData;
+                if(paragraphElements) 
+                {
+                     paragraphElements.innerHTML = paragraph.content;
+                }               
+            }, this);
       }
-  });   
 }
 //it loads the file from paragraph.json
 function loadContent()
@@ -90,23 +86,24 @@ function initialize()
      //Loads the initialize function after loading all the html functions 
     window.addEventListener("load",initialize);
 
-    //Footer Part
-    loadFooter();
-    function loadFooter()
-    {
-    console.info("Footer is loading...");
 
-    // step 1 - Creates the XHR object
-     let XHR = new XMLHttpRequest();
+                    //Footer Part
+loadFooter();
+function loadFooter()
+{
+  console.info("Footer is loading...");
 
-    // step 2 - Configures the message
-     XHR.open("GET", "./Views/partials/footer.html");
+  // step 1 - Creates the XHR object
+  let XHR = new XMLHttpRequest();
 
-    // step 3 - Executes the request
-    XHR.send();
+  // step 2 - Configures the message
+  XHR.open("GET", "./Views/partials/footer.html");
 
-    XHR.addEventListener("readystatechange", function()
-    {
+  // step 3 - Executes the request
+  XHR.send();
+
+  XHR.addEventListener("readystatechange", function()
+  {
       if((XHR.readyState === 4) && (XHR.status === 200))
       {
           let footer = document.getElementsByTagName("footer")[0];
@@ -114,6 +111,5 @@ function initialize()
           let footerData = XHR.responseText;
           footer.innerHTML = footerData;
       }
-    });   
+  });   
 }
- 
